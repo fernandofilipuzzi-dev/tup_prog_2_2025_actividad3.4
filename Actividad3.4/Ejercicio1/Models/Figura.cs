068b8305ed91b0cf37fed5115215a9d0ef329dbe
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-08-28 22:35:29 -0300

    iniciando repo

---
+++ Actividad3.4/Ejercicio1/Models/Figura.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Ejercicio1.Models;
+
+abstract public class Figura :IComparable
+{
+    abstract public string Describir();
+    
+    virtual public double CalcularVolumen()
+    {
+        return 0;
+    }
+
+    public override string ToString()
+    {
+        return Describir();
+    }
+
+    public int CompareTo(object otra)
+    {
+        Figura otraFigura=otra as Figura;
+        if (otraFigura != null)
+        {
+            return CalcularVolumen().CompareTo(otraFigura.CalcularVolumen());
+        }
+        return -1;
+    }
+}
---
+++ Actividad3.4/Ejercicio1/Models/Ortoedro.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Ejercicio1.Models;
+
+public class Ortoedro : Figura
+{
+    public double Ancho { get; set; }
+    public double Alto { get; set; }
+    public double Largo { get; set; }
+
+    public Ortoedro(double ancho, double alto, double largo)
+    {
+        this.Ancho = ancho;
+        Alto = alto;
+        Largo = largo;
+    }
+
+    override public double CalcularVolumen()
+    {
+        return Ancho*Alto*Largo;
+    }
+
+    public override string Describir()
+    {
+        return $"Ortoedro: Ancho: {Ancho}";
+    }
+
+   
+}
